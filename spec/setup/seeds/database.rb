3c0abd2dbbdeba86d55213f704391012d1c4e32b
Lief Nikkel <lief.nikkel@youdo.co.nz>
2018-07-04 14:21:34 +1200

    use en-us string for test db

--- spec/setup/seeds/database.rb
+++ spec/setup/seeds/database.rb
@@ -2,5 +2,5 @@ db_config = YAML::load(File.open(File.expand_path('../../database.yml', __FILE__
 db_config_admin = db_config.merge({database: 'postgres', schema_search_path: 'public'})
 ActiveRecord::Base.establish_connection(db_config_admin)
 ActiveRecord::Base.connection.drop_database(db_config["database"])
-ActiveRecord::Base.connection.create_database(db_config["database"])
+ActiveRecord::Base.connection.create_database(db_config["database"], db_config)
 ActiveRecord::Base.establish_connection(db_config)
